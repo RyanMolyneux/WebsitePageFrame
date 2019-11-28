5e6cc97d548806e493dde0d2dd90a74b7851a79b
Ryan <ryanmolyneux07@gmail.com>
2019-11-28 23:29:36 +0000

    1. GPLV3 License applied.

--- src/general/threads/Thread.js
+++ src/general/threads/Thread.js
@@ -1,3 +1,22 @@
+/*
+ *    This library provides a way to allow browser based programs to have cross parent to child window communication through code.
+ *    Copyright (C) 2019  Ryan Molyneux
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 function _setCodeToBeExecuted(codeToBeExecuted) {
 
     if ( !(codeToBeExecuted instanceof Object) ) {
